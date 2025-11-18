5ae50e3785bc33532018730d9abdcc7eec4ceb4c
lishen <carlosleey95@gmail.com>
2025-11-18 15:37:57 +0800

    添加环境变量，控制Hdp刷新

--- src/gda/queue_pair.cpp
+++ src/gda/queue_pair.cpp
@@ -70,7 +70,11 @@ QueuePair::QueuePair(struct ibv_pd* pd, int gda_provider) {
     fetching_atomic_freelist->push_back(fetching_atomic + i);
   }
 
-  hipDeviceGetAttribute((int*)&gpuHdpReg, hipDeviceAttributeHdpMemFlushCntl, deviceId);
+  // 控制是否需要刷新hdp
+  const char* hdpDisableFlushEnv = getenv("ROCSHMEM_DISABLE_HDP_FLUSH");
+  if (hdpDisableFlushEnv == NULL || strcmp(hdpDisableFlushEnv, "1") != 0) {
+    hipDeviceGetAttribute((int*)&gpuHdpReg, hipDeviceAttributeHdpMemFlushCntl, deviceId);
+  }
 
   /* Set Correct opcodes for each NIC */
   switch (gda_provider) {
