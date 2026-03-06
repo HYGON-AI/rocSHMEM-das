4b9065a91d230d8fcecd81bfee7876fd099ef813
Aurelien Bouteiller <abouteil@amd.com>
2026-03-06 11:02:22 -0500

    [rocshmem]: add flood-amo tester (#3653)
    * WIP creating flood amo tester
    
    * Copy the grid_barrier from the MoE example to utils.h
    
    * Add flood amoadd tester
    
    * Integrate the flood-amoadd tester to the harness
    
    * Add flood-amoadd to CI/driver script
    
    * Add case fadd
    
    * Remove if 0 code
    
    * Per it verification, in comments some alternatives for loading the local
    
    * Add a co-residency test, remove test failure of verif
    
    * Better protection against overflow for num_timed_msgs
    
    * remove not-yet-implemented fcswap flood tester
    
    * Update grid barrier plus inter-node synchronization for correct data verification
    
    * Do not add floodFCSwap for now.
    
    * Add floodWaitAmo test case
    
    * recombine floodwaitamo and floodamo into a single tester
    fix issues found by copilot
    
    ---------
    
    Co-authored-by: Avinash <avinash.kethineedi@amd.com>

--- tests/functional_tests/tester.hpp
+++ tests/functional_tests/tester.hpp
@@ -127,6 +127,10 @@ enum TestType {
   FloodGetNBITestType = 87,
   FloodGTestType = 88,
   HipModuleInitTestType = 89,
+  FloodAddTestType = 90,
+  FloodFAddTestType = 91,
+  FloodWaitAmoTestType = 92,
+
 
   // Self-developed test cases start from 200 to avoid community conflicts.
   DefaultCtx_WAVEPutNBITestType_DP = 200,
@@ -164,8 +168,8 @@ class Tester {
   virtual void verifyResults(uint64_t size) = 0;
 
   size_t max_msg_size = 0;
-  int num_msgs = 0;
-  int num_timed_msgs = 0;
+  size_t num_msgs = 0;
+  size_t num_timed_msgs = 0;
   int num_loops = 0;
   int size_factor = 1;
   int bw_factor = 1;
