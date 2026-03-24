b4a4d7c12a3a5ff4a60c690a112f3467aca6d4f1
lijian <34831075+lijian0711@users.noreply.github.com>
2026-03-24 15:31:59 +0800

    解决deepep低时延卡住问题

--- src/gda/mlx5/queue_pair_mlx5_multiqp.cpp
+++ src/gda/mlx5/queue_pair_mlx5_multiqp.cpp
@@ -11,35 +11,21 @@ __device__ void QueuePair::mlx5_post_wqe_rma_dp_single_lane(int32_t size, uintpt
 
   // wqe_idx is the logical WQE id that wraps at 0xFFFF, sq_idx is the index into the actual SQ
   uint16_t wqe_idx = mlx5_sq.tail;
-  uint16_t sq_idx = wqe_idx % mlx5_sq.depth;
-
-  // can we inline the data into the WQE?
-  bool send_inline = gda_mlx5_wqe_rma::can_inline(opcode, size, inline_threshold);
+  uint16_t sq_idx = wqe_idx & (mlx5_sq.depth - 1);
 
   // construct the WQE on the stack
   gda_mlx5_wqe wqe{wqe_idx, opcode, qp_num, 0,
-                   raddr, rkey, laddr, lkey, static_cast<uint32_t>(size), send_inline};
+                   raddr, rkey, laddr, lkey, static_cast<uint32_t>(size), false};
 
   // copy to SQ
   mlx5_sq.buf[sq_idx] = wqe;
-
+  
   // increment tail counter
   mlx5_sq.tail += 1;
   mlx5_sq.post += 1;
 
-  if (gpuHdpReg != nullptr) {
-    __hip_atomic_store(reinterpret_cast<uint32_t*>(gpuHdpReg), (uint32_t)0x1, __ATOMIC_SEQ_CST, __HIP_MEMORY_SCOPE_SYSTEM);
-  }
-
-  uint64_t wqe_writed {0};
-  do {
-    wqe_writed = __hip_atomic_load(&sq_wqe_writed, __ATOMIC_RELAXED, __HIP_MEMORY_SCOPE_AGENT);
-  } while (wqe_writed != (mlx5_sq.post - 1));
-
   // ring doorbell for this WQE
-  mlx5_ring_doorbell(mlx5_sq.tail, wqe, false);
-
-  __hip_atomic_store(&sq_wqe_writed, mlx5_sq.post, __ATOMIC_RELAXED, __HIP_MEMORY_SCOPE_AGENT);
+  mlx5_ring_doorbell(mlx5_sq.tail, mlx5_sq.buf[sq_idx]);
 
   // release SQ lock
   release_lock(&mlx5_sq.lock);
@@ -52,11 +38,10 @@ __device__ void QueuePair::mlx5_post_wqe_amo_dp_single_lane(int32_t size, uintpt
 
   uint64_t* atomic_laddr = nonfetching_atomic;
   uint32_t atomic_lkey = nonfetching_atomic_lkey;
-  
 
   // wqe_idx is the logical WQE id that wraps at 0xFFFF, sq_idx is the index into the actual SQ
   uint16_t wqe_idx = mlx5_sq.tail;
-  uint16_t sq_idx = wqe_idx % mlx5_sq.depth;
+  uint16_t sq_idx = wqe_idx & (mlx5_sq.depth - 1);
 
   // construct the WQE on the stack
   gda_mlx5_wqe wqe{wqe_idx, opcode, qp_num, 0,
@@ -71,19 +56,8 @@ __device__ void QueuePair::mlx5_post_wqe_amo_dp_single_lane(int32_t size, uintpt
   mlx5_sq.tail += 1;
   mlx5_sq.post += 1;
 
-  if (gpuHdpReg != nullptr) {
-    __hip_atomic_store(reinterpret_cast<uint32_t*>(gpuHdpReg), (uint32_t)0x1, __ATOMIC_SEQ_CST, __HIP_MEMORY_SCOPE_SYSTEM);
-  }
-
-  uint64_t wqe_writed {0};
-  do {
-    wqe_writed = __hip_atomic_load(&sq_wqe_writed, __ATOMIC_RELAXED, __HIP_MEMORY_SCOPE_AGENT);
-  } while (wqe_writed != (mlx5_sq.post - 1));
-
-  // ring doorbell for this WQE (note: need to check this for correctness)
-  mlx5_ring_doorbell(mlx5_sq.tail, wqe, false);
-
-  __hip_atomic_store(&sq_wqe_writed, mlx5_sq.post, __ATOMIC_RELAXED, __HIP_MEMORY_SCOPE_AGENT);
+  // ring doorbell for this WQE
+  mlx5_ring_doorbell(mlx5_sq.tail, mlx5_sq.buf[sq_idx]);
 
   // release SQ lock
   release_lock(&mlx5_sq.lock);
