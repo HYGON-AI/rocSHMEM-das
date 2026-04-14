340d0b10bfd93bdc9a0eecb022c0fcbefe7ddb84
Anatolii Rozanov <anatolii.rozanov@amd.com>
2026-04-14 14:45:59 +0000

    [rocshmem] Remove multi_ctx_put_tester test as duplicate

--- tests/functional_tests/tester_arguments.cpp
+++ tests/functional_tests/tester_arguments.cpp
@@ -246,7 +246,6 @@ void TesterArguments::get_arguments() {
     case FloodFAddTestType:
     case FloodWaitAmoTestType:
     case DeviceBitcodeTestType:
-    case MultiCtxPutTestType:
       requires_two_pes = false;
       break;
     default:
