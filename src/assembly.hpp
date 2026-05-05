d6a06f2e6f5631e03d3a794b76c0dbc6f6dc3a8c
amirsojoodi <amir.sojoodi@amd.com>
2026-05-05 18:53:09 +0000

    Dev: add nt hints to large puts only

--- src/assembly.hpp
+++ src/assembly.hpp
@@ -329,19 +329,19 @@ __device__ __forceinline__ void put_asm([[maybe_unused]] uint8_t* src,
       *dst128 = val128;
 #endif
 #if defined(__gfx90a__) || defined(__gfx1100__)
-      asm volatile("flat_store_dwordx4 %0, %1, glc"
+      asm volatile("flat_store_dwordx4 %0, %1, glc slc"
                    :
                    : "v"(dst), "v"(val128)
                    : "memory");
 #endif
 #if defined(__gfx942__) || defined(__gfx950__)
-      asm volatile("flat_store_dwordx4 %0, %1, sc0 sc1"
+      asm volatile("flat_store_dwordx4 %0, %1, sc0 sc1 nt"
                    :
                    : "v"(dst), "v"(val128)
                    : "memory");
 #endif
 #if defined(__gfx1201__)
-      asm volatile("flat_store_b128 %0, %1, scope:SCOPE_SYS"
+      asm volatile("flat_store_b128 %0, %1, scope:SCOPE_SYS th:TH_STORE_NT_RT"
                    :
                    : "v"(dst), "v"(val128)
                    : "memory");
