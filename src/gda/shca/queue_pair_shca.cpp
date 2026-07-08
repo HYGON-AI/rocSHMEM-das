090f083978fd1070eecc684ad079372448ce257e
lijian <34831075+lijian0711@users.noreply.github.com>
2026-07-08 17:03:04 +0800

    fix normal mode hang when no use xdp.
    Signed-off-by: lijian <34831075+lijian0711@users.noreply.github.com>

--- src/gda/shca/queue_pair_shca.cpp
+++ src/gda/shca/queue_pair_shca.cpp
@@ -170,11 +170,10 @@ __device__ __forceinline__ void QueuePair::shca_build_rma_wqe(
   if (size <= inline_threshold && opcode == gda_op_rdma_write) {
     seg_build.update_inl_data_seg(reinterpret_cast<const void*>(laddr), size);
   } else {
-    // seg_build.update_data_seg(laddr, size, lkey);
-    if (flag == 0)
-      seg_build.update_data_seg(laddr, size, lkey_hdp);
-    else
-      seg_build.update_data_seg(laddr, size, lkey);
+    // if (flag == 0) // TODO: normal mode dont use XDP for now.
+    //   seg_build.update_data_seg(laddr, size, lkey_hdp);
+    // else
+    seg_build.update_data_seg(laddr, size, lkey);
   }
 }
 
@@ -269,7 +268,7 @@ __device__ __forceinline__ void QueuePair::shca_build_amo_wqe(
   SegmentBuilder_SHCA seg_build(my_sq_index, sq_buf);
   seg_build.update_ctrl_seg(my_sq_counter, opcode, 0, qp_num,
                             SHCA_WQE_CTRL_CQ_ALWAYS, 4, 0, 0);
-  seg_build.update_raddr_seg(raddr, rkey_hdp);
+  seg_build.update_raddr_seg(raddr, rkey);
   seg_build.update_atomic_seg(atomic_data, atomic_cmp);
 
   if (fetching) {
